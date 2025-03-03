160529b8b45f0589cc331b6855bce6b73601c6c8
Rostyslav Svistula <rostyslav.svistula@gmail.com>
2025-03-03 21:09:02 +0100

    init project

---
+++ src/main/java/sk/tuke/fpa_tool_ws/FpaToolWsApplication.java
@@ -0,0 +1,13 @@
+package sk.tuke.fpa_tool_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpaToolWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpaToolWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/tuke/fpa_tool_ws/FpaToolWsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.tuke.fpa_tool_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpaToolWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
