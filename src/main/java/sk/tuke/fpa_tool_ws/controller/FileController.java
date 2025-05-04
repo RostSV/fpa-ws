c1fbc05b0d23591f4945441cd12652b9e30772a4
Rostyslav Svistula <rostyslav.svistula@gmail.com>
2025-05-04 12:50:10 +0200

    add endpoint to retrieve example file based on type

--- src/main/java/sk/tuke/fpa_tool_ws/controller/FileController.java
+++ src/main/java/sk/tuke/fpa_tool_ws/controller/FileController.java
@@ -1,12 +1,15 @@
 package sk.tuke.fpa_tool_ws.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import sk.tuke.fpa_tool_ws.dto.ApiResponse;
 import sk.tuke.fpa_tool_ws.dto.request.SaveXlsRequest;
+import sk.tuke.fpa_tool_ws.enums.FileType;
 import sk.tuke.fpa_tool_ws.service.ExcelReaderService;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 @RequestMapping("/api/files")
@@ -16,6 +19,17 @@ public class FileController {
 
     public FileController(ExcelReaderService excelReaderService) {
         this.excelReaderService = excelReaderService;
+    }
+
+    @GetMapping("/example/{type}")
+    public ApiResponse<byte[]> getExample(@PathVariable String type) throws IOException {
+        FileType fileType = FileType.valueOf(type.toUpperCase());
+        ClassPathResource fileResource = switch (fileType) {
+            case XLS -> new ClassPathResource("templates/excel-table-template.xls");
+            default -> throw new IllegalArgumentException("Unsupported file type: " + type);
+        };
+
+        return new ApiResponse<>(200, "File byte[] data", Files.readAllBytes(fileResource.getFile().toPath()));
     }
 
     @PostMapping("/upload-calculations")
---
+++ src/main/java/sk/tuke/fpa_tool_ws/enums/FileType.java
@@ -0,0 +1,7 @@
+package sk.tuke.fpa_tool_ws.enums;
+
+public enum FileType {
+    PDF,
+    XLS,
+    CSV
+}
